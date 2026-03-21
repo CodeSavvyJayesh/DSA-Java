d38c3e4d5f788c7723fdffe3df0edec8e069990e
Jayesh <jayeshdhamal327@gmail.com>
2026-03-22 00:14:01 +0530

    Number System representations in code

---
+++ BasicConcepts/NumberSystemExamples.java
@@ -0,0 +1,16 @@
+package BasicConcepts;
+
+public class NumberSystemExamples {
+    public static void main(String [] args) {
+        int a= 10;    // decimal    (0,9)
+        int b = 0b1010;      // binary     (0,1)
+        int c = 0xAB;        // hexadecimal (0-F)
+        int d = 0050;        // octal   (0-7)
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
