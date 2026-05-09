53b8350ca7b526b60dfb95664f0a666efb651620
Jayesh <jayeshdhamal327@gmail.com>
2026-05-09 23:51:46 +0530

    2Sum interview commit

---
+++ interviewQuestions/twoSum.java
@@ -0,0 +1,41 @@
+package interviewQuestions;
+import java.util.*;
+public class twoSum {
+    public static int [] twoSumm(int [] nums,int target)
+    {
+         int n = nums.length;
+         HashMap<Integer,Integer> map = new HashMap<>();
+         for(int i=0;i<n;i++)
+         {
+              int num = nums[i];
+              int complement = target - num;
+              if(map.containsKey(complement))
+              {
+                   return new int[]{map.get(complement),i};
+              }
+              else {
+                  map.put(num,i);
+              }
+         }
+         return new int[]{-1,-1};
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of an array : ");
+        int n = sc.nextInt();
+        int [] nums = new int[n];
+        System.out.println("Enter the array elements ");
+        for(int i=0;i<n;i++)
+        {
+            nums[i] = sc.nextInt();
+        }
+        System.out.println("Enter the target element ");
+        int target = sc.nextInt();
+        int [] ans = twoSumm(nums,target);
+        for(int i=0;i<ans.length;i++)
+        {
+            System.out.println(ans[i]);
+        }
+
+    }
+}
